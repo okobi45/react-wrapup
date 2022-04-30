c250e0c26cdeb0ba3355e1bd13f9205098139fb7
Okobi Eigbe <okobieigbe@gmail.com>
2022-04-30 18:19:52 +0100

    file added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,22 @@ import React from "react";
 export default function Footer(){
     return(
         <div className="footer">
-            <p>All Rights Reserved - WrapUP 2022</p>
+            <div className="footer-logo">
+                <p>WrapUP</p>
+            </div>
+            <div className="footer-nav">
+                <nav className="nav">
+                    <ul>
+                        <li><a href="#home" className="active">home</a></li>
+                        <li><a href="#about">about</a></li>
+                        <li><a href="#service">service</a></li>
+                        <li><a href="#contact">contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div className="footer-credit">
+                <p>Copyright 2022 by WrapUP. All Rights Reserved.</p>
+            </div>
         </div>
     )
 }
